b41c3a689c99ff35a1798bd034b0927b279e4154
doodger <jcrondyt@gmail.com>
2020-07-24 09:49:03 -0400

    template work

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+import datetime
+from django.utils import timezone
 # Create your models here.
+class Question(models.Model):
+	question_text = models.CharField(max_length = 200)
+	pub_date = models.DateTimeField('date published') #human readable name given as "date published"
+
+	#method to turn this into a str
+	def __str__(self):
+		return self.question_text
+
+	def was_published_recently(self):
+		return self.pub_date >= timezone.now()
+
+datetime.timedelta(days=1)
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE) #on deletion, delete things related to this
+	choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default=0)
+
+	#method to turn this into a str
+	def __str__(self):
+		return self.choice_text
+
--- polls/urls.py
+++ polls/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('<int:question_id>/', views.detail, name='detail')
+	##Other stuff done during tutorial
+	#path('', views.index, name='index'),
+	#exemple: /polls/5
+	#path('<int:question_id>/', views.detail, name='detail'),
+	#exemple /polls/5/results
+	#path('<int:question_id>/results/', views.results, name='results'),
+	#exemple /polls/5/vote
+	#path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
